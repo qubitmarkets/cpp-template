86b97a5df3d658bcedaafc24dafdc8f8fa55a417
Richard Geary <richard@qubit-markets.com>
2025-06-04 10:43:23 +0000

    Fix for build_clang, build_cmake Improve build-run

--- 3rdparty/build_clang.sh
+++ 3rdparty/build_clang.sh
@@ -4,6 +4,8 @@ CLANG_VER=${CLANG_VER-$1}
 CLANG_MAJOR_VER=${CLANG_VER%%.*}
 DESTDIR=${DESTDIR-/opt/infra.1}
 
+cd $(dirname $0)
+
 set -eu
 
 if [[ -z $CLANG_VER ]]; then
@@ -13,7 +15,7 @@ if [[ -z $CLANG_VER ]]; then
 fi
 
 # Install clang
-if [[ -e /usr/bin/clang++-$CLANG_VER ]]; then exit 0; fi
+#if [[ -e /usr/bin/clang++-$CLANG_VER ]]; then exit 0; fi
 
 if [[ -e /usr/bin/apt ]]; then
     sudo apt-get install -y libstdc++-12-dev
@@ -27,15 +29,21 @@ fi
 cd llvm-project
 git checkout release/${CLANG_MAJOR_VER}.x
 
-mkdir -p build && cd build
+BUILD_DIR=build-${CLANG_MAJOR_VER}
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
 cmake -G Ninja ../llvm \
   -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;lldb;polly;mlir" \
+  -DCLANG_DEFAULT_PIE_ON_LINUX=OFF \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$DESTDIR
 
 ninja
 sudo ninja install
 sudo ln -nsf clang-${CLANG_MAJOR_VER} $DESTDIR/bin/clang++-${CLANG_MAJOR_VER}
+if [[ ! -e /usr/bin/clang-$CLANG_MAJOR_VER ]]; then
+  sudo ln -nfs $DESTDIR/bin/clang-${CLANG_MAJOR_VER} /usr/bin/clang-${CLANG_MAJOR_VER}
+  sudo ln -nfs $DESTDIR/bin/clang++-${CLANG_MAJOR_VER} /usr/bin/clang++-${CLANG_MAJOR_VER}
+fi
 
 touch /tmp/empty.c
 $DESTDIR/bin/clang-$CLANG_MAJOR_VER /tmp/empty.c -fsyntax-only
--- 3rdparty/build_cmake.sh
+++ 3rdparty/build_cmake.sh
@@ -3,6 +3,8 @@
 VER=4.0.2
 DESTDIR=/opt/infra.1
 
+cd $(dirname $0)
+
 wget https://github.com/Kitware/CMake/releases/download/v${VER}/cmake-${VER}.tar.gz -O /tmp/cmake-${VER}.tar.gz
 tar -xzf /tmp/cmake-${VER}.tar.gz
 rm /tmp/cmake-${VER}.tar.gz
