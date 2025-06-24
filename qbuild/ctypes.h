b5b36ca3f06ab756fa29a26318248b07083eaa1e
Richard Geary <richard@qubit-markets.com>
2025-06-24 20:04:55 +0800

    Add u128 and u256 types

--- qbuild/ctypes.h
+++ qbuild/ctypes.h
@@ -12,3 +12,25 @@ using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
+
+struct alignas(16) u128 {
+    u128(unsigned char v[32]) {
+        u64* v64 = (u64*)v;
+        _v[0] = v64[0];
+        _v[1] = v64[1];
+    }
+
+    u64 _v[2];
+};
+
+struct alignas(32) u256 {
+    u256(unsigned char v[32]) {
+        u64* v64 = (u64*)v;
+        _v[0] = v64[0];
+        _v[1] = v64[1];
+        _v[2] = v64[2];
+        _v[3] = v64[3];
+    }
+
+    u64 _v[4];
+};
