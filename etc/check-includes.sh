6c1f75eeec9fabccfc9341bf46d3ed04382c7048
Richard Geary <richard@qubit-markets.com>
2025-08-26 17:24:47 +0800

    Fix etc/check-includes

--- etc/check-includes.sh
+++ etc/check-includes.sh
@@ -4,20 +4,23 @@
 
 set -euo pipefail
 
-include_hierarchy=(qbuild mem io app)
+include_hierarchy=(qbuild mem shm io nexus app feed)
+src=qcore
 
 n=${#include_hierarchy[@]}
 n=$(($n - 1))
+rv=0
 
 for i in $(seq 0 $n); do
     a=${include_hierarchy[$i]}
-    if [[ ! -d "src/$a" ]]; then
+    if [[ ! -d "$src/$a" ]]; then
         continue
     fi
+    #echo "check-includes: $a/"
     for j in $(seq $(($i + 1)) $n); do
         b=${include_hierarchy[$j]}
-        grep -r '# *include ["<]'"$b"'/' "src/$a" && echo "No $b/ includes allowed in src/$a"
+        grep -r '# *include ["<]'"$b"'/' "$src/$a" && echo "No $b/ includes allowed in $src/$a" && rv=1
     done
 done
 
-exit 0
+exit $rv
